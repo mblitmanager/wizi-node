cc993eff42ffc7b87d19279de68dd85487ea3b3a
MBL IT Manager <mblitmanager@gmail.com>
2026-01-10 12:43:43 +0300

    refactor: remove AchievementController interface definition

--- dist/app.module.d.ts
+++
@@ -1,5 +0,0 @@
-import { NestModule, MiddlewareConsumer } from "@nestjs/common";
-export declare class AppModule implements NestModule {
-    constructor();
-    configure(consumer: MiddlewareConsumer): void;
-}
